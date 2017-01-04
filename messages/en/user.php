f2332eab3dce82bf98ffdbef901b1a953989034e
Evgeny Zakirov <box@flyleaf.su>
2017-01-04 10:52:02 +0800

    update

--- messages/en/user.php
+++ messages/en/user.php
@@ -2,6 +2,7 @@
 return [
     'TITLE_LOGIN'=>'Authorization',
     'TITLE_RESET_PASSWORD'=>'Reset password request',
+    'TITLE_UPDATE'=>'Update profile',
 
     'PLEASE_FILL_FOR_RESET_REQUEST'=>'Please fill email below, to reset request password reset link',
 
@@ -11,12 +12,17 @@ return [
 
     'CONTACT_NAME_EMAIL'=>'Username or Email',
     'USER_PASSWORD'=>'Password',
+    'USER_PASSWORD_REPEAT'=>'Repeat password',
 
     'NAV_LOGIN'=>'Login',
     'NAV_SIGNUP'=>'Sign up',
+
     'BUTTON_SEND'=>'Send',
+    'BUTTON_SAVE'=>'Save',
 
     'LINK_RESET_PASSWORD'=>'Reset password',
+    'LINK_SOCIAL_TO_USER'=>'Link social accounts',
 
     'ERROR_WRONG_USERNAME_OR_PASSWORD'=>'Wrong username or password',
+
 ];
